742d293b0b33574a59162e079edbebab71c2bc37
Oliver Lasse Eierstrand <oliv1115@edu.easj.dk>
2020-02-18 12:29:17 +0100

    Add project files.

---
+++ EchoClient/EchoClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace EchoClient
+{
+    class EchoClient
+    {
+        static void Main(string[] args)
+        {
+           
+            Start();
+
+        }
+
+        public static void Start()
+        {
+            TcpClient socket = new TcpClient("localhost", 7777);
+            Stream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+            string line = sr.ReadLine();
+
+            Console.WriteLine(line);
+        }
+
+
+    }
+}
---
+++ EchoServer/EchoServer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    class EchoServer
+    {
+        static void Main(string[] args)
+        {
+            //Opret server
+            IPAddress ip = IPAddress.Parse("192.168.24.205");
+            //Opret adresse eller port
+            TcpListener serverSocket = new TcpListener(ip, 7777);
+            //starter server
+            serverSocket.Start();
+            Console.WriteLine("Server Started");
+
+            //loop åbner for mulighed for at connecte flere clients
+            do
+            {
+                Task.Run(() =>
+                {
+                    //Venter på connection før den aktiverer
+                    TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+                    Console.WriteLine("Server activated & Connected");
+                    //kalder metoden DoClient
+                    DoClient(connectionSocket);
+
+                   });
+
+            } while (true);
+
+            serverSocket.Stop();
+            Console.WriteLine("Server stopped & Disconnected");
+        }
+
+        public static void DoClient(TcpClient socket)
+        {
+            Stream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            string message = sr.ReadLine();
+            string answer = "";
+
+            while (message != null && message != "")
+            {
+                Console.WriteLine("Client: " + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+
+            ns.Close();
+            socket.Close();
+
+        }
+    }
+}
